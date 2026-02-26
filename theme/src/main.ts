8dcfcbe8c462a7e6d61d6ad9349f76d77ab97d21
Pradyuman Aggarwal <pradyumana@Pradyumans-MacBook-Pro-3.local>
2026-02-26 20:49:25 +0530

    Adding styles changes for home page

--- theme/src/main.ts
+++ theme/src/main.ts
@@ -13,3 +13,9 @@ import "./main.css";
 // Javascript or Typescript
 import "./**/*.js";
 import "./**/*.ts";
+
+// Flameback feedback widget
+const flamebackScript = document.createElement('script');
+flamebackScript.src = 'https://flameback.adobe.io/widget/flameback-widget-loader.js?siteId=46b038a7-9878-4c69-ac4c-001e290cb88a';
+flamebackScript.defer = true;
+document.head.appendChild(flamebackScript);
